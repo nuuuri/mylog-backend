c3c8f39db0f1d85d6e44796aa794dc2345eb437a
nuuuri <sbfl125@gmail.com>
2022-11-23 21:35:08 +0900

    [feat] BaseTimeEntity,User 추가

---
+++ src/main/java/io/github/nuuuri/mylog/data/entity/User.java
@@ -0,0 +1,39 @@
+package io.github.nuuuri.mylog.data.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_uuid", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(length = 20, nullable = false)
+    private String password;
+
+    @Column(length = 15, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Builder
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+}
